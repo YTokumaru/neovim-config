b03d3082c62064dc66214ebf67691d149c7f916e
YTokumaru <ytokumaru16@gmail.com>
2023-03-22 21:52:42 +0900

    copilot starts suggesting at 0 words

--- lua/nvim-cmp_conf.lua
+++ lua/nvim-cmp_conf.lua
@@ -10,7 +10,7 @@ cmp.setup {
     },
     mapping = require('maps').mapCmpKey(),
     sources = {
-        { name =  'copilot', group_index = 2 },
+        { name =  'copilot', group_index = 2, keyword_length = 0 },
         { name = 'nvim_lsp', group_index = 2 },
         { name = 'luasnip', group_index = 2 },
         { name = 'path', group_index = 2},
